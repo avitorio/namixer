73bcd54174738a416191ee89b89f40d18d008a8b
Andre Vitorio <andre@vitorio.net>
2021-12-02 16:07:02 -0300

    feat: only connect to socket if in browser

--- src/config/web-sockets.ts
+++ src/config/web-sockets.ts
@@ -2,12 +2,14 @@ import { createContext } from 'react'
 import { io } from 'socket.io-client'
 let STRAPI_ENDPOINT: string | undefined
 
+const isBrowser = typeof window !== 'undefined'
+
 if (process.env.NODE_ENV === 'development') {
-  STRAPI_ENDPOINT = 'http://localhost:1337'
+  STRAPI_ENDPOINT = isBrowser ? 'http://localhost:1337' : ''
 } else if (process.env.NODE_ENV === 'test') {
   STRAPI_ENDPOINT = ''
 } else if (process.env.NODE_ENV === 'production') {
-  STRAPI_ENDPOINT = process.env.NEXT_PUBLIC_API_URL
+  STRAPI_ENDPOINT = isBrowser ? process.env.NEXT_PUBLIC_API_URL : ''
 }
 
 export const socket = io(STRAPI_ENDPOINT || '')
